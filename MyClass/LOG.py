958de89b01466624a1b1168aac46d8145a7ae698
azhuge233 <azhuge786698994@gmail.com>
2020-10-03 10:21:08 +0800

    Update LOG.py

--- MyClass/LOG.py
+++ MyClass/LOG.py
@@ -1,8 +1,24 @@
 import logging
 
 
-logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# log format
+format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+format_without_levelname = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+
+# the root logger
 logger = logging.getLogger()
-handler = logging.FileHandler("log.txt")
-handler.setLevel(logging.NOTSET)
-logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+# file Log
+f_handler = logging.FileHandler("log.txt")
+f_handler.setLevel(logging.INFO)
+f_handler.setFormatter(format)
+
+# console Log
+s_handler = logging.StreamHandler()
+s_handler.setLevel(logging.WARNING)
+s_handler.setFormatter(format_without_levelname)
+
+# add handler to logger
+logger.addHandler(f_handler)
+logger.addHandler(s_handler)
